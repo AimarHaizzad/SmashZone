e97edba7b419789b22b219d9957f534167f391cb
Aimar Haizzad <haizzadaimar@gmail.com>
2025-11-01 16:59:08 +0800

    Add redirect and web.config for Azure

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-// Entry point for Azure App Service when deploying repository root.
-// Delegate to Laravel's public index.
-require __DIR__ . '/public/index.php';
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * This file redirects to the public directory
+ */
 
-
+header('Location: /public/');
+exit;
