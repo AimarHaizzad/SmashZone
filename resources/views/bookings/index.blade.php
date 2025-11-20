e7bec2925c6191116080014063a73bd5098134a0
Aimar Haizzad <haizzadaimar@gmail.com>
2025-11-21 04:47:15 +0800

    t27

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -183,8 +183,20 @@
                                                 <svg class="h-3 w-3 sm:h-4 sm:w-4 md:h-5 md:w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                                                 </svg>
-                                                <span class="hidden sm:inline">@if($isStart)My Booking@else{{ $duration }}h Booking@endif</span>
-                                                <span class="sm:hidden">@if($isStart)Mine@else{{ $duration }}h@endif</span>
+                                                <span class="hidden sm:inline">
+                                                    @if($isStart)
+                                                        My Booking
+                                                    @else
+                                                        {{ $duration }}h Booking
+                                                    @endif
+                                                </span>
+                                                <span class="sm:hidden">
+                                                    @if($isStart)
+                                                        Mine
+                                                    @else
+                                                        {{ $duration }}h
+                                                    @endif
+                                                </span>
                                             </div>
                                             @if($isStart)
                                                 <div class="text-[10px] sm:text-xs text-blue-600 hidden sm:block">
@@ -205,8 +217,20 @@
                                                 <svg class="h-3 w-3 sm:h-4 sm:w-4 md:h-5 md:w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z" />
                                                 </svg>
-                                                <span class="hidden sm:inline">@if($isStart)Booked@else{{ $duration }}h Booked@endif</span>
-                                                <span class="sm:hidden">@if($isStart)Booked@else{{ $duration }}h@endif</span>
+                                                <span class="hidden sm:inline">
+                                                    @if($isStart)
+                                                        Booked
+                                                    @else
+                                                        {{ $duration }}h Booked
+                                                    @endif
+                                                </span>
+                                                <span class="sm:hidden">
+                                                    @if($isStart)
+                                                        Booked
+                                                    @else
+                                                        {{ $duration }}h
+                                                    @endif
+                                                </span>
                                             </div>
                                             @if($isStart)
                                                 <div class="text-[10px] sm:text-xs text-red-600 hidden sm:block">
