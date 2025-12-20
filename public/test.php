43251120f1efe840e1ce1912883c930c5fb6ea62
Aimar Haizzad <haizzadaimar@gmail.com>
2025-12-21 02:38:44 +0800

    remove unwanted file

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<h1>SmashZone Test Page</h1>";
-echo "<p>If you can see this, the deployment is working!</p>";
-echo "<p>Current time: " . date('Y-m-d H:i:s') . "</p>";
-echo "<p>PHP Version: " . phpversion() . "</p>";
-echo "<p>Server: " . $_SERVER['SERVER_SOFTWARE'] . "</p>";
-?>
-
